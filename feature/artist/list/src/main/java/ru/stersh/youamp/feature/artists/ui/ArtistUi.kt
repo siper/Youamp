5016dd99431724670d10b52df35e79b33628925c
Kirill Zhukov <siper13@gmail.com>
2024-06-05 21:40:38 +0300

    Fix gonic issues (#121)

--- feature/artist/list/src/main/java/ru/stersh/youamp/feature/artists/ui/ArtistUi.kt
+++ feature/artist/list/src/main/java/ru/stersh/youamp/feature/artists/ui/ArtistUi.kt
@@ -3,6 +3,6 @@ package ru.stersh.youamp.feature.artists.ui
 internal data class ArtistUi(
     val id: String,
     val name: String,
-    val albumCount: Int,
+    val albumCount: Int?,
     val artworkUrl: String?
 )
