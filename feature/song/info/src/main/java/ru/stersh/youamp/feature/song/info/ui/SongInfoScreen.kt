34c1aef0a395f67bc3bfef11b0cc7513c10fc07d
Kirill Zhukov <siper13@gmail.com>
2025-01-26 16:15:51 +0300

    Remove song from queue

--- feature/song/info/src/main/java/ru/stersh/youamp/feature/song/info/ui/SongInfoScreen.kt
+++ feature/song/info/src/main/java/ru/stersh/youamp/feature/song/info/ui/SongInfoScreen.kt
@@ -1,27 +1,14 @@
 package ru.stersh.youamp.feature.song.info.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Album
 import androidx.compose.material.icons.rounded.MusicVideo
 import androidx.compose.material.icons.rounded.PersonSearch
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.QueuePlayNext
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -30,18 +17,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
 import ru.stersh.youamp.core.api.provider.ApiProvider
 import ru.stersh.youamp.core.ui.Artwork
-import ru.stersh.youamp.core.ui.SkeletonLayout
+import ru.stersh.youamp.core.ui.SongMenu
 import ru.stersh.youamp.core.ui.YouampPlayerTheme
 import ru.stersh.youamp.feature.song.info.R
 import ru.stersh.youamp.shared.player.queue.AudioSource
@@ -62,7 +45,7 @@ fun SongInfoScreen(
 
     var state by remember(showAlbum) { mutableStateOf(SongInfoStateUi(showAlbum = showAlbum)) }
 
-    LaunchedEffect("load_track_state") {
+    LaunchedEffect(id) {
         val api = apiProvider.getApi()
         val song = api.getSong(id)
 
@@ -96,158 +79,6 @@ fun SongInfoScreen(
 }
 
 @Composable
-private fun Progress(
-    state: SongInfoStateUi
-) {
-    SkeletonLayout {
-        Column(
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .windowInsetsPadding(WindowInsets.navigationBars),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Row(
-                modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                SkeletonItem(
-                    modifier = Modifier.size(64.dp)
-                )
-                Column(
-                    modifier = Modifier.weight(1f),
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    SkeletonItem(
-                        modifier = Modifier.size(height = 12.dp, width = 80.dp)
-                    )
-                    SkeletonItem(
-                        modifier = Modifier.size(height = 12.dp, width = 68.dp)
-                    )
-                }
-            }
-
-            HorizontalDivider(
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-
-            Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.padding(vertical = 12.dp)
-            ) {
-                SkeletonItem(
-                    modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .size(height = 32.dp, width = 264.dp)
-                        .fillMaxWidth()
-                )
-                SkeletonItem(
-                    modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .size(height = 32.dp, width = 164.dp)
-                        .fillMaxWidth()
-                )
-                SkeletonItem(
-                    modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .size(height = 32.dp, width = 200.dp)
-                        .fillMaxWidth()
-                )
-                if (state.showAlbum) {
-                    SkeletonItem(
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp)
-                            .size(height = 32.dp, width = 300.dp)
-                            .fillMaxWidth()
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun Content(
-    state: SongInfoStateUi,
-    onDismiss: () -> Unit,
-    onOpenAlbum: (albumId: String) -> Unit,
-    onOpenArtist: (artistId: String) -> Unit,
-    onPlay: () -> Unit,
-    onPlayNextInQueue: () -> Unit,
-) {
-    Column(
-        modifier = Modifier
-            .padding(top = 8.dp)
-            .windowInsetsPadding(WindowInsets.navigationBars),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Row(
-            modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Artwork(
-                artworkUrl = state.artworkUrl,
-                placeholder = Icons.Rounded.Album,
-                modifier = Modifier.size(64.dp)
-            )
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                state.title?.let { title ->
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
-                state.artist?.let { artist ->
-                    Text(
-                        text = artist,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.secondary
-                    )
-                }
-            }
-        }
-
-        HorizontalDivider(
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-
-        MenuItem(
-            icon = Icons.Rounded.PlayArrow,
-            title = stringResource(R.string.play_title),
-            onClick = onPlay
-        )
-        MenuItem(
-            icon = Icons.Rounded.QueuePlayNext,
-            title = stringResource(R.string.play_next_in_queue_title),
-            onClick = onPlayNextInQueue
-        )
-        if (state.showAlbum && state.albumId != null) {
-            MenuItem(
-                icon = Icons.Rounded.MusicVideo,
-                title = stringResource(R.string.go_to_album_title),
-                onClick = {
-                    onOpenAlbum(state.albumId)
-                    onDismiss()
-                }
-            )
-        }
-        state.artistId?.let { artistId ->
-            MenuItem(
-                icon = Icons.Rounded.PersonSearch,
-                title = stringResource(R.string.go_to_artist_title),
-                onClick = {
-                    onOpenArtist(artistId)
-                    onDismiss()
-                }
-            )
-        }
-    }
-}
-
-@Composable
 private fun SongInfoScreen(
     state: SongInfoStateUi,
     onDismiss: () -> Unit,
@@ -256,54 +87,92 @@ private fun SongInfoScreen(
     onPlay: () -> Unit,
     onPlayNextInQueue: () -> Unit,
 ) {
-    Surface(
-        color = MaterialTheme.colorScheme.surface
-    ) {
-        if (state.progress) {
-            Progress(
-                state = state
+    SongMenu(
+        progress = state.progress,
+        artwork = {
+            Artwork(
+                artworkUrl = state.artworkUrl,
+                placeholder = Icons.Rounded.Album,
+                modifier = Modifier.fillMaxSize()
             )
-        } else {
-            Content(
-                state = state,
-                onDismiss = onDismiss,
-                onOpenAlbum = onOpenAlbum,
-                onOpenArtist = onOpenArtist,
-                onPlay = onPlay,
-                onPlayNextInQueue = onPlayNextInQueue
+        },
+        title = {
+            state.title?.let {
+                Text(text = it)
+            }
+        },
+        artist = {
+            state.artist?.let {
+                Text(text = it)
+            }
+        }
+    ) {
+        item(
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.PlayArrow,
+                    contentDescription = stringResource(R.string.play_title),
+                )
+            },
+            title = {
+                Text(text = stringResource(R.string.play_title))
+            },
+            onClick = {
+                onPlay()
+                onDismiss()
+            }
+        )
+        item(
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.QueuePlayNext,
+                    contentDescription = stringResource(R.string.play_next_in_queue_title),
+                )
+            },
+            title = {
+                Text(text = stringResource(R.string.play_next_in_queue_title))
+            },
+            onClick = {
+                onPlayNextInQueue()
+                onDismiss()
+            }
+        )
+
+        if (state.showAlbum && state.albumId != null) {
+            item(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Rounded.MusicVideo,
+                        contentDescription = stringResource(R.string.go_to_album_title)
+                    )
+                },
+                title = {
+                    Text(text = stringResource(R.string.go_to_album_title))
+                },
+                onClick = {
+                    onOpenAlbum(state.albumId)
+                    onDismiss()
+                }
             )
         }
-    }
-}
 
-@Composable
-private fun MenuItem(
-    icon: ImageVector,
-    title: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val shape = remember { RoundedCornerShape(16.dp) }
-    Row(
-        modifier = Modifier
-            .clip(shape)
-            .clickable(onClick = onClick)
-            .padding(vertical = 12.dp, horizontal = 24.dp)
-            .then(modifier),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = title,
-            tint = MaterialTheme.colorScheme.secondary,
-            modifier = Modifier
-        )
-        Text(
-            text = title,
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.weight(1f)
-        )
+        state.artistId?.let { artistId ->
+            item(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Rounded.PersonSearch,
+                        contentDescription = stringResource(R.string.go_to_artist_title)
+                    )
+                },
+                title = {
+                    Text(text = stringResource(R.string.go_to_artist_title))
+                },
+                onClick = {
+                    onOpenArtist(artistId)
+                    onDismiss()
+                }
+            )
+        }
     }
 }
 
