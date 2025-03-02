b9eb70c1360b39a93d354ef4bdfcaa3d87852573
Kirill Zhukov <siper13@gmail.com>
2025-03-02 23:15:40 +0300

    Fix preview in feature modules

--- feature/player/queue/build.gradle.kts
+++ feature/player/queue/build.gradle.kts
@@ -1,22 +1,12 @@
-import com.android.build.api.dsl.androidLibrary
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.kotlin.composeCompiler)
-    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.android.library)
 }
 
 kotlin {
-    androidLibrary {
-        namespace = "ru.stersh.youamp.feature.player.queue"
-        compileSdk = libs.versions.android.compileSdk
-            .get()
-            .toInt()
-        minSdk = libs.versions.android.minSdk
-            .get()
-            .toInt()
-    }
+    androidTarget()
 
     sourceSets {
         commonMain {
@@ -32,7 +22,28 @@ kotlin {
                 implementation(libs.koin.compose)
                 implementation(libs.lifecycle.viewmodel)
                 implementation(libs.kotlinx.collectionsImmutable)
+                implementation(compose.preview)
+                implementation(compose.uiTooling)
             }
         }
+    }
+}
+
+android {
+    namespace = "ru.stersh.youamp.feature.player.queue"
+    compileSdk = libs.versions.android.compileSdk
+        .get()
+        .toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk
+            .get()
+            .toInt()
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
