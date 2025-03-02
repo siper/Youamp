e3708069d8de24bc20d31e33627d031acf0a1460
Kirill Zhukov <siper13@gmail.com>
2025-03-02 19:11:51 +0300

    KMP song

---
+++ feature/song/favorites/src/commonMain/kotlin/ru/stresh/youamp/feature/song/favorites/ui/FavoriteSongsScreen.kt
@@ -0,0 +1,260 @@
+package ru.stresh.youamp.feature.song.favorites.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MusicNote
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.collections.immutable.persistentListOf
+import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.viewmodel.koinViewModel
+import ru.stersh.youamp.core.ui.Artwork
+import ru.stersh.youamp.core.ui.BackNavigationButton
+import ru.stersh.youamp.core.ui.EmptyLayout
+import ru.stersh.youamp.core.ui.ErrorLayout
+import ru.stersh.youamp.core.ui.HeaderLayout
+import ru.stersh.youamp.core.ui.HeaderTitle
+import ru.stersh.youamp.core.ui.PlayAllButton
+import ru.stersh.youamp.core.ui.PlayShuffledButton
+import ru.stersh.youamp.core.ui.SkeletonLayout
+import youamp.feature.song.favorites.generated.resources.Res
+import youamp.feature.song.favorites.generated.resources.favorite_songs_title
+
+@Composable
+fun FavoriteSongsScreen(
+    onSongClick: (id: String) -> Unit,
+    onBackClick: () -> Unit
+) {
+    val viewModel: FavoriteSongsViewModel = koinViewModel()
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    FavoriteSongsScreen(
+        state = state,
+        onPlayAll = viewModel::playAll,
+        onPlayShuffled = viewModel::playShuffled,
+        onRetry = viewModel::retry,
+        onRefresh = viewModel::refresh,
+        onSongClick = onSongClick,
+        onBackClick = onBackClick
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FavoriteSongsScreen(
+    state: StateUi,
+    onPlayAll: () -> Unit,
+    onPlayShuffled: () -> Unit,
+    onRetry: () -> Unit,
+    onRefresh: () -> Unit,
+    onSongClick: (id: String) -> Unit,
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    BackNavigationButton(onClick = onBackClick)
+                }
+            )
+        }
+    ) {
+        PullToRefreshBox(
+            isRefreshing = state.isRefreshing,
+            onRefresh = onRefresh,
+            modifier = Modifier.padding(it)
+        ) {
+            when {
+                state.progress -> {
+                    Progress()
+                }
+
+                state.error -> {
+                    ErrorLayout(onRetry = onRetry)
+                }
+
+                state.data != null && state.data.songs.isEmpty() -> {
+                    EmptyLayout(
+                        modifier = Modifier.verticalScroll(
+                            state = rememberScrollState()
+                        )
+                    )
+                }
+
+                state.data?.songs != null -> {
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.background)
+                            .fillMaxSize()
+                    ) {
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            item(
+                                contentType = "header",
+                                key = "header"
+                            ) {
+                                HeaderLayout(
+                                    title = {
+                                        HeaderTitle(text = stringResource(Res.string.favorite_songs_title))
+                                    },
+                                    actions = {
+                                        PlayAllButton(
+                                            onClick = onPlayAll
+                                        )
+                                        PlayShuffledButton(
+                                            onClick = onPlayShuffled
+                                        )
+                                    }
+                                )
+                            }
+                            items(
+                                items = state.data.songs,
+                                contentType = { "song" },
+                                key = { "song_${it.id}" }
+                            ) { song ->
+                                FavoriteSongItem(
+                                    song = song,
+                                    onClick = { onSongClick(song.id) }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Progress() {
+    SkeletonLayout {
+        repeat(10) {
+            ListItem(
+                headlineContent = {
+                    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                        SkeletonItem(modifier = Modifier.size(width = 130.dp, height = 16.dp))
+                        SkeletonItem(modifier = Modifier.size(width = 200.dp, height = 16.dp))
+                    }
+                },
+                leadingContent = {
+                    SkeletonItem(modifier = Modifier.size(48.dp))
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun FavoriteSongItem(
+    song: SongUi,
+    onClick: () -> Unit
+) {
+    ListItem(
+        headlineContent = {
+            Text(
+                text = song.title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        supportingContent = {
+            song.artist?.let {
+                Text(
+                    text = it,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        },
+        leadingContent = {
+            Artwork(
+                artworkUrl = song.artworkUrl,
+                placeholder = Icons.Rounded.MusicNote,
+                modifier = Modifier.size(48.dp)
+            )
+        },
+        modifier = Modifier.clickable(onClick = onClick)
+    )
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun FavoriteSongsScreenPreview() {
+    MaterialTheme {
+        val songs = persistentListOf(
+            SongUi(
+                id = "1",
+                title = "Best song in the world 1",
+                artist = "Best artist in the world 1",
+                artworkUrl = null
+            ),
+            SongUi(
+                id = "2",
+                title = "Best song in the world 2",
+                artist = "Best artist in the world 1",
+                artworkUrl = null
+            ),
+            SongUi(
+                id = "3",
+                title = "Best song in the world 3",
+                artist = "Best artist in the world 1",
+                artworkUrl = null
+            ),
+            SongUi(
+                id = "4",
+                title = "Best song in the world 4",
+                artist = "Best artist in the world 1",
+                artworkUrl = null
+            ),
+            SongUi(
+                id = "5",
+                title = "Best song in the world 5",
+                artist = "Best artist in the world 1",
+                artworkUrl = null
+            ),
+        )
+        val state = StateUi(
+            progress = false,
+            isRefreshing = false,
+            error = false,
+            data = DataUi(
+                songs = songs
+            )
+        )
+        FavoriteSongsScreen(
+            state = state,
+            onPlayAll = {},
+            onPlayShuffled = {},
+            onRetry = {},
+            onRefresh = {},
+            onSongClick = {},
+            onBackClick = {}
+        )
+    }
+}
---
+++ feature/song/info/src/commonMain/kotlin/ru/stersh/youamp/feature/song/info/ui/SongInfoViewModel.kt
@@ -0,0 +1,77 @@
+package ru.stersh.youamp.feature.song.info.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.touchlab.kermit.Logger
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ru.stresh.youamp.core.api.ApiProvider
+import ru.stresh.youamp.shared.favorites.SongFavoritesStorage
+import ru.stresh.youamp.shared.queue.AudioSource
+import ru.stresh.youamp.shared.queue.PlayerQueueAudioSourceManager
+
+internal class SongInfoViewModel(
+    private val apiProvider: ApiProvider,
+    private val playerQueueAudioSourceManager: PlayerQueueAudioSourceManager,
+    private val songFavoritesStorage: SongFavoritesStorage
+) : ViewModel() {
+
+    val state: StateFlow<State>
+        get() = _state
+
+    private val _state = MutableStateFlow(State())
+    private var getSongJob: Job? = null
+
+    fun loadSongInfo(songId: String, showAlbum: Boolean) {
+        _state.value = State(showAlbum = showAlbum)
+        getSongJob?.cancel()
+        getSongJob = viewModelScope.launch {
+            try {
+                val api = apiProvider.getApi()
+                val song = api.getSong(songId).data.song
+                _state.update {
+                    it.copy(
+                        artworkUrl = api.getCoverArtUrl(song.coverArt),
+                        title = song.title,
+                        artist = song.artist,
+                        artistId = song.artistId,
+                        albumId = song.albumId,
+                        progress = false,
+                        error = false,
+                        favorite = song.starred != null,
+                    )
+                }
+            } catch (error: Exception) {
+                if (error is CancellationException) throw error
+                _state.update {
+                    it.copy(
+                        progress = false,
+                        error = true,
+                    )
+                }
+            }
+        }
+    }
+
+    fun play(songId: String) = viewModelScope.launch {
+        playerQueueAudioSourceManager.playSource(AudioSource.Song(songId))
+    }
+
+    fun playAfterCurrent(songId: String) = viewModelScope.launch {
+        playerQueueAudioSourceManager.addAfterCurrent(AudioSource.Song(songId))
+    }
+
+    fun addToFavorites(songId: String) = viewModelScope.launch {
+        runCatching { songFavoritesStorage.setSongFavorite(songId, true) }
+            .onFailure { Logger.w(it) { "Filed to like song" } }
+    }
+
+    fun removeFromFavorites(songId: String) = viewModelScope.launch {
+        runCatching { songFavoritesStorage.setSongFavorite(songId, false) }
+            .onFailure { Logger.e(it) { "Filed to dislike song" } }
+    }
+}
