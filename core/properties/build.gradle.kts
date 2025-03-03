897b79fa0f3ddc67d75821f09e3da56a7610d049
Kirill Zhukov <siper13@gmail.com>
2025-03-03 15:31:42 +0300

    Refactor: Rename and update Android Kotlin Multiplatform Library plugin (#424)

--- core/properties/build.gradle.kts
+++ core/properties/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.android.kotlinMultiplatformLibrary)
 }
 
 kotlin {
