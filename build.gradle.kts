f5a577fe8614db9af9687573ffbb5aab1b71b578
Kirill Zhukov <siper13@gmail.com>
2025-03-03 15:14:32 +0300

    Refactor: Rename and update Android Kotlin Multiplatform Library plugin
    Renamed the `android.kotlin.multiplatform.library` plugin alias to `android.kotlinMultiplatformLibrary` for consistency and updated its usage across multiple `build.gradle.kts` files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.composeCompiler) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.android.kotlinMultiplatformLibrary) apply false
 }
 
 tasks.register("clean", Delete::class) {
