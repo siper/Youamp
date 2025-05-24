4606ab8a3956b4c2da727c27fba9d0fbf0298f8c
Kirill Zhukov <siper13@gmail.com>
2025-05-24 10:10:55 +0300

    Release 2.0.0-beta08

--- buildSrc/src/main/kotlin/ru/stersh/youamp/AppBuildInfo.kt
+++ buildSrc/src/main/kotlin/ru/stersh/youamp/AppBuildInfo.kt
@@ -2,8 +2,8 @@ package ru.stersh.youamp
 
 object AppBuildInfo {
 
-    const val VERSION_NAME = "2.0.0-beta07"
-    const val VERSION_CODE = 24
+    const val VERSION_NAME = "2.0.0-beta08"
+    const val VERSION_CODE = 25
     const val PACKAGE_NAME = "ru.stersh.youamp"
 
     val CLEAR_VERSION_NAME = clearVersionName()
