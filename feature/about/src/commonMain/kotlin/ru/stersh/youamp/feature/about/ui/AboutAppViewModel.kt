5e1c0da94848651a1157a57424b1d8947c2f5945
Kirill Zhukov <siper13@gmail.com>
2025-06-01 14:06:33 +0300

    feat: open links multiplatform

--- feature/about/src/commonMain/kotlin/ru/stersh/youamp/feature/about/ui/AboutAppViewModel.kt
+++ feature/about/src/commonMain/kotlin/ru/stersh/youamp/feature/about/ui/AboutAppViewModel.kt
@@ -3,22 +3,11 @@ package ru.stersh.youamp.feature.about.ui
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import ru.stersh.youamp.core.properties.app.AppProperties
 import ru.stersh.youamp.core.properties.app.AppPropertiesStorage
 
 internal class AboutAppViewModel(appPropertiesStorage: AppPropertiesStorage) : ViewModel() {
-    private val _state = MutableStateFlow(appPropertiesStorage.getAppProperties().toState())
+    private val _state = MutableStateFlow(appPropertiesStorage.getAppProperties().toUiState())
 
     val state: StateFlow<AboutStateUi>
         get() = _state
-
-    private fun AppProperties.toState(): AboutStateUi {
-        return AboutStateUi(
-            name = name,
-            version = version,
-            githubUri = githubUrl,
-            fdroidUri = fdroidUrl,
-            crwodinUri = crwodinUrl
-        )
-    }
 }
---
+++ feature/about/src/commonMain/kotlin/ru/stersh/youamp/feature/about/ui/Mapper.kt
@@ -0,0 +1,14 @@
+package ru.stersh.youamp.feature.about.ui
+
+import ru.stersh.youamp.core.properties.app.AppProperties
+
+
+internal fun AppProperties.toUiState(): AboutStateUi {
+    return AboutStateUi(
+        name = name,
+        version = version,
+        githubUri = githubUrl,
+        fdroidUri = fdroidUrl,
+        crwodinUri = crwodinUrl
+    )
+}
