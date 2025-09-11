b03f0fc580ff6d6c75f3c30b6efb21835242d1de
Kirill Zhukov <siper13@gmail.com>
2025-09-11 18:37:27 +0300

    chore: release 2.0.0

--- buildSrc/src/main/kotlin/ru/stersh/youamp/AppBuildInfo.kt
+++ buildSrc/src/main/kotlin/ru/stersh/youamp/AppBuildInfo.kt
@@ -1,8 +1,8 @@
 package ru.stersh.youamp
 
 object AppBuildInfo {
-    const val VERSION_NAME = "2.0.0-beta10"
-    const val VERSION_CODE = 27
+    const val VERSION_NAME = "2.0.0"
+    const val VERSION_CODE = 28
     const val PACKAGE_NAME = "ru.stersh.youamp"
 
     val CLEAR_VERSION_NAME = clearVersionName()
